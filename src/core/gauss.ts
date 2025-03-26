62e1940a440487c1e6a870416c134be92397c122
Arthaud Proust <arthaudp33@gmail.com>
2025-03-26 18:07:40 +0100

    :pencil: Comment algo

--- src/core/gauss.ts
+++ src/core/gauss.ts
@@ -4,11 +4,12 @@ export class UnresolvableSystemError extends Error {}
 export class Systeme {
   constructor(private matrice: number[][]) {
     if (matrice.some((ligne) => ligne.length !== matrice.length + 1)) {
-      throw new InvalidSystemError()
+      throw new InvalidSystemError("Il doit y avoir autant de lignes que de variables. Chaque ligne doit comporter les coefficients des variables puis le résultat")
     }
   }
 
   get matrice_triangle() {
+    // Copie de la matrice pour ne pas modifier  l'original
     const matrice = this.matrice.map((ligne) => ligne.slice())
 
     // Mettre à zéro le coefficient en faisant une combinaison linéaire avec la ligne d'au dessus
@@ -22,7 +23,7 @@ export class Systeme {
       return systeme
     }
 
-    // Création de la matrice triangulaire en appliquant le pivot de gauss
+    // Création de la matrice triangulaire supérieure 
     for (let ligne = 1; ligne < matrice.length; ligne++) {
       for (let colonne = 0; colonne < ligne; colonne++) {
         mettre_a_zero(matrice, ligne, colonne)
@@ -37,6 +38,7 @@ export class Systeme {
 
     const solutions: number[] = Array(mat_triangle.length)
 
+    // Résoudre les équations pas à pas de bas en haut
     for (let ligne_i = mat_triangle.length - 1; ligne_i >= 0; ligne_i--) {
       const ligne = mat_triangle[ligne_i]
 
